2aa1a38be635bf52d5d1e81c03541e568ceb94ba
Nate <eclypso@aol.com>
2019-03-01 16:01:01 -0500

    added html and css files

--- src/main/java/org/wecancodeit/springproject/repositories/ProductRepository.java
+++ src/main/java/org/wecancodeit/springproject/repositories/ProductRepository.java
@@ -11,27 +11,3 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 	
 }
 
-// The following code is no longer necessary thanks to CRUD Repos.
-//public class ProductRepository {
-//
-//	private List<Product> products;
-//
-//	public ProductRepository(List<Product> inventory) {
-//		this.products = inventory;
-//	}
-//
-//	public List<Product> getInventory() {
-//		return products;
-//	}
-//	
-//	public void addProduct(Product product) {
-//		products.add(product);
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "ProductRepository [products=" + products + "]";
-//	}
-//	
-//	
-//}
